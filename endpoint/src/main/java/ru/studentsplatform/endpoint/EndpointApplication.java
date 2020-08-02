e1cac92f0996b404b88f21015ea45e16c5bff06f
Krylov Sergey <kssergeyru@gmail.com>
2020-08-02 09:32:45 +0300

    Настроен Zipkin + Zuul

--- endpoint/src/main/java/ru/studentsplatform/endpoint/EndpointApplication.java
+++ endpoint/src/main/java/ru/studentsplatform/endpoint/EndpointApplication.java
@@ -1,9 +1,11 @@
 package ru.studentsplatform.endpoint;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -13,5 +15,10 @@ import org.springframework.context.annotation.ComponentScan;
 public class EndpointApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EndpointApplication.class, args);
+	}
+
+	@Bean
+	public Sampler defaultSampler(){
+		return Sampler.ALWAYS_SAMPLE;
 	}
 }
